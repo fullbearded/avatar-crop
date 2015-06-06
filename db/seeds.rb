82afe6ce32678b0904e7ba7bc122b5779fa8fc36
Hongda Hu <jerrery520@gmail.com>
2015-06-06 12:19:54 +0800

    [jerry] add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Image.create(ori_image: File.new(Rails.root.join('app','assets', 'images', 'avatar.jpg')))
